58c11071f274087567188f851608bb7fb09e1dc4
WilliamC111 <110690003+WilliamC111@users.noreply.github.com>
2023-09-24 08:13:46 -0500

    Update SearchResultWindow.java

--- src/view/SearchResultWindow.java
+++ src/view/SearchResultWindow.java
@@ -25,4 +25,6 @@ public class SearchResultWindow extends JFrame {
     public void displayResults(String results) {
         resultArea.setText(results);
     }
+    public void displayResults(List<Book> foundBooks) {
+}
 }
